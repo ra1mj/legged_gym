1877c79b352911e21c2f186818004f364bc9129a
ra1mj <1965534511@qq.com>
2025-03-17 21:12:32 +0800

    dcu_yaml 3.17

--- legged_gym/envs/NOVA_V0.1/nova_env.py
+++ legged_gym/envs/NOVA_V0.1/nova_env.py
@@ -27,6 +27,7 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 # Copyright (c) 2021 ETH Zurich, Nikita Rudin
+# Copyright (c) 2025 ra1mj
 from legged_gym import LEGGED_GYM_ROOT_DIR, envs
 from time import time
 from warnings import WarningMessage
@@ -39,7 +40,7 @@ from isaacgym import gymtorch, gymapi, gymutil
 import torch
 from torch import Tensor
 from typing import Tuple, Dict
-from legged_gym.envs.base.base_task import BaseTask
+from base.base_task import BaseTask
 from legged_gym.utils.terrain import Terrain
 from legged_gym.utils.math import quat_apply_yaw, wrap_to_pi, torch_rand_sqrt_float
 from legged_gym.utils.helpers import class_to_dict
